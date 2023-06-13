a09788d80beaafbba1e709c8f17af0b17686b565
Scott Stroz <scott.stroz@oracle.com>
2023-06-13 09:07:29 -0400

    Added additional calls to API

--- src/main/java/com/boyzoid/controller/ScoreController.java
+++ src/main/java/com/boyzoid/controller/ScoreController.java
@@ -4,6 +4,7 @@ import com.boyzoid.service.ScoreService;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.mysql.cj.xdevapi.JsonArray;
+import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.Nullable;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.MediaType;
@@ -25,12 +26,58 @@ public class ScoreController {
         this.scoreService = scoreService;
     }
 
-    @Get(value = "/list{/limit}", produces = MediaType.APPLICATION_JSON)
-    public HttpResponse<Object> list(@Nullable Integer limit ) throws JsonProcessingException {
-        ArrayList<Object> scores = scoreService.getScores(!Objects.isNull(limit) ? limit : defaultLimit );
+    @Get(value = "/list", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object> listAll(@Nullable Integer limit ) throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getAllScores();
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    @Get(value = "/list/{limit}{/offset}", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object> list(Integer limit, @Nullable Integer offset ) throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getScores(limit, !Objects.isNull(offset) ? offset : 0 );
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    @Get(value = "/bestScores{/limit}", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object> bestScores(@Nullable Integer limit ) throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getBestScores(!Objects.isNull(limit) ? limit : defaultLimit );
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    @Get(value = "/getRoundsUnderPar", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object> roundsUnderPar( ) throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getRoundsUnderPar();
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    @Get(value = "/getByScore{/score}", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object>getByScore(@Nullable Integer score ) throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getByScore(!Objects.isNull(score) ? score : 45 );
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    @Get(value = "/getByGolfer{/lastName}", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object>getByGolfer(@Nullable String lastName ) throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getByGolfer(!Objects.isNull(lastName) ? lastName : "" );
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    @Get(value = "/getCourseScoringData", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object>getCourseScoringData() throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getCourseScoringData();
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    @Get(value = "/getAggregateCourseScore", produces = MediaType.APPLICATION_JSON)
+    public HttpResponse<Object>getAggregateCourseScore() throws JsonProcessingException {
+        ArrayList<Object> scores = scoreService.getAggregateCourseScore();
+        return HttpResponse.ok(getResult(scores));
+    }
+
+    private static LinkedHashMap getResult(ArrayList<?> scores){
         LinkedHashMap result = new LinkedHashMap();
         result.put("count", scores.size());
         result.put("scores", scores);
-        return HttpResponse.ok(result);
+        return result;
     }
 }
--- src/main/java/com/boyzoid/service/ScoreService.java
+++ src/main/java/com/boyzoid/service/ScoreService.java
@@ -1,42 +1,169 @@
 package com.boyzoid.service;
 
+import com.boyzoid.config.DocumentStoreConfig;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mysql.cj.xdevapi.*;
-import io.micronaut.context.annotation.Value;
 import jakarta.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Singleton
 public class ScoreService {
     private final ObjectMapper objectMapper = new ObjectMapper();
     private final ClientFactory clientFactory = new ClientFactory();
+    private final DocumentStoreConfig documentStoreConfig;
+    private Client cli;
     private String url;
     private String schema;
     private String collection;
-    private Client cli;
 
-    public ScoreService(@Value("${demo.user}") String user,
-                        @Value("${demo.password}") String password,
-                        @Value("${demo.host}") String host,
-                        @Value("${demo.port}") String port,
-                        @Value("${demo.schema}") String schema,
-                        @Value("${demo.collection}") String collection) {
-        this.url = "mysqlx://" + user + ":" + password + "@" + host + ":" + port + "/";
-        this.schema = schema;
-        this.collection = collection;
+    public ScoreService(DocumentStoreConfig documentStoreConfig) {
+        this.documentStoreConfig = documentStoreConfig;
+        this.url = "mysqlx://" + documentStoreConfig.getUser() +
+                ":" + documentStoreConfig.getPassword() +
+                "@" + documentStoreConfig.getHost() +
+                ":" + documentStoreConfig.getPort().toString() +
+                "/" + documentStoreConfig.getSchema();
+        this.schema = documentStoreConfig.getSchema();
+        this.collection = documentStoreConfig.getCollection();
         this.cli = clientFactory.getClient(this.url, "{\"pooling\":{\"enabled\":true, \"maxSize\":8,\"maxIdleTime\":30000, \"queueTimeout\":10000} }");
     }
 
-    public ArrayList<Object> getScores(Integer limit) throws JsonProcessingException {
+    public ArrayList<Object> getAllScores() throws JsonProcessingException {
+        Session session = getSession();
+        Schema schema = session.getSchema(this.schema);
+        Collection col = schema.getCollection(this.collection);
+        DocResult result = col.find().execute();
+        ArrayList<Object> docs = cleanResults(result.fetchAll());
+        session.close();
+        return docs;
+    }
+
+    public ArrayList<Object> getScores(Integer limit, Integer offset) throws JsonProcessingException {
        Session session = getSession();
        Schema schema = session.getSchema(this.schema);
        Collection col = schema.getCollection(this.collection);
-       DocResult result = col.find().limit(limit).execute();
+       DocResult result = col.find()
+               .limit(limit)
+               .offset(offset)
+               .execute();
        ArrayList<Object> docs = cleanResults(result.fetchAll());
        session.close();
        return docs;
+    }
+
+    public ArrayList<Object> getBestScores(Integer limit) throws JsonProcessingException {
+        Session session = getSession();
+        Schema schema = session.getSchema(this.schema);
+        Collection col = schema.getCollection(this.collection);
+        DocResult result = col.find()
+                .fields("firstName as firstName, " +
+                        "lastName as lastName, " +
+                        "score as score, " +
+                        "course as course, " +
+                        "`date` as datePlayed")
+                .sort("score asc, `date` desc")
+                .limit(limit)
+                .execute();
+        ArrayList<Object> docs = cleanResults(result.fetchAll());
+        session.close();
+        return docs;
+    }
+
+    public ArrayList<Object> getRoundsUnderPar() throws JsonProcessingException {
+        Session session = getSession();
+        Schema schema = session.getSchema(this.schema);
+        Collection col = schema.getCollection(this.collection);
+        DocResult result = col.find("score < course.par")
+                .fields("firstName as firstName, " +
+                        "lastName as lastName, " +
+                        "score as score, " +
+                        "course.name as course, " +
+                        "`date` as datePlayed")
+                .sort("`date` desc")
+                .execute();
+        ArrayList<Object> docs = cleanResults(result.fetchAll());
+        session.close();
+        return docs;
+    }
+
+    public ArrayList<Object> getByScore(Integer score) throws JsonProcessingException {
+        Session session = getSession();
+        Schema schema = session.getSchema(this.schema);
+        Collection col = schema.getCollection(this.collection);
+        DocResult result = col.find("score = :scoreParam")
+                .bind("scoreParam", score)
+                .fields("concat(firstName, \" \", lastName) as golfer, " +
+                        "score as score, " +
+                        "course.name as course, " +
+                        "`date` as datePlayed")
+                .sort("score asc, `date` desc")
+                .execute();
+        ArrayList<Object> docs = cleanResults(result.fetchAll());
+        session.close();
+        return docs;
+    }
+
+    public ArrayList<Object> getByGolfer(String lastName) throws JsonProcessingException {
+        Session session = getSession();
+        Schema schema = session.getSchema(this.schema);
+        Collection col = schema.getCollection(this.collection);
+        DocResult result = col.find("lower(lastName) like :lastNameParam")
+                .bind("lastNameParam", lastName.toLowerCase()+"%")
+                .fields("concat(firstName, \" \", lastName) as golfer, " +
+                        "score as score, " +
+                        "course.name as course, " +
+                        "`date` as datePlayed")
+                .sort("lastName, firstName, `date` desc")
+                .execute();
+        ArrayList<Object> docs = cleanResults(result.fetchAll());
+        session.close();
+        return docs;
+    }
+
+    public ArrayList<Object> getCourseScoringData() throws JsonProcessingException {
+        Session session = getSession();
+        Schema schema = session.getSchema(this.schema);
+        Collection col = schema.getCollection(this.collection);
+        DocResult result = col.find()
+                .fields("course.name as courseName, " +
+                        "round(avg(score), 2)  as avg, " +
+                        "min(cast(score as unsigned)) as lowestScore," +
+                        "max(cast(score as unsigned)) as highestScore, " +
+                        "count(score) as numberOfRounds")
+                .groupBy("course.name")
+                .sort("course.name desc")
+                .execute();
+        ArrayList<Object> docs = cleanResults(result.fetchAll());
+        session.close();
+        return docs;
+    }
+    
+    public ArrayList<Object> getAggregateCourseScore() throws JsonProcessingException {
+        Session session = getSession();
+        String sql = "WITH aggScores AS " +
+                "            (SELECT doc ->> '$.course.name' course, " +
+                "                MIN(score)              minScore, " +
+                "                MAX(score)              maxScore, " +
+                "                number " +
+                "            FROM scores, " +
+                "                JSON_TABLE(doc, '$.holeScores[*]' " +
+                "                    COLUMNS ( " +
+                "                        score INT PATH '$.score', " +
+                "                        number INT PATH '$.number')) AS scores " +
+                "            GROUP BY course, number " +
+                "            ORDER BY course, number) " +
+                "        SELECT JSON_OBJECT('courseName', course, 'bestScore', sum(minScore)) agg" +
+                "        FROM aggScores " +
+                "        GROUP BY course " +
+                "        ORDER BY course;";
+        SqlStatement query = session.sql(sql);
+        SqlResult result = query.execute();
+        ArrayList<Object> docs = cleanSqlResults(result.fetchAll(), "agg");
+        session.close();
+        return docs;
     }
 
     private Session getSession(){
@@ -47,6 +174,14 @@ public class ScoreService {
         ArrayList<Object> cleaned = new ArrayList<>();
         for( DbDoc doc : docs){
             cleaned.add( objectMapper.readTree(doc.toString()));
+        }
+        return cleaned;
+    }
+
+    private ArrayList<Object> cleanSqlResults(List<Row> rows, String field) throws JsonProcessingException {
+        ArrayList<Object> cleaned = new ArrayList<>();
+        for( Row row : rows){
+            cleaned.add( objectMapper.readTree(row.getString(field)));
         }
         return cleaned;
     }
